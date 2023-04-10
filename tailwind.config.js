45e948d0be55f2e5a9fc598f0e0dba15228b461d
PVIdubs <nluismi80@gmail.com>
2023-04-10 14:47:58 -0400

    adding base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,46 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      archivo: ["Archivo", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        sigma: "#1183ea",
+        salert: "#aa1530",
+        solive: {
+          100: "#edf5d4",
+          200: "#c2d199",
+          300: "#8cb059",
+          400: "#30590a",
+        },
+        sgreen: {
+          100: "#ccede9",
+          200: "#82c9c9",
+          300: "#2eab9e",
+          400: "#005454",
+        },
+        sblue: {
+          100: "#bae0fa",
+          200: "#73baeb",
+          300: "#0d66a8",
+          400: "#003d75",
+        },
+        scopper: {
+          100: "#f7dec4",
+          200: "#f09c40",
+          300: "#bf6b0d",
+          400: "#914a05",
+        },
+        sgray: {
+          100: "#dcdcdc",
+          200: "#959595",
+          300: "#595959",
+          400: "#333333",
+        },
+      },
+    },
   },
   plugins: [],
 };
-
