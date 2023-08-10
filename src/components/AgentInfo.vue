b03504ed64d534d7b124bd01aeb6759e60c6560f
PVIdubs <nluismi80@gmail.com>
2023-08-10 16:05:14 -0400

    minor changes

--- src/components/AgentInfo.vue
+++ src/components/AgentInfo.vue
@@ -253,21 +253,6 @@
       </div>
     </form>
 
-    <!-- My Posts -->
-    <!-- <section class="mb-20 w-full">
-      <div class="mb-4 flex w-full items-center gap-[6px]">
-        <h3 class="text-base font-semibold">Tus publicaciones ({{ props.posts.length }})</h3>
-        <div class="h-[1px] grow border-t border-black"></div>
-      </div>
-      <ul class="space-y-4">
-        <li v-for="(item, index) in props.posts" :key="index">
-          <RouterLink :to="`/post/${item._id}`">
-            <PostTableItem :post="item" />
-          </RouterLink>
-        </li>
-      </ul>
-    </section> -->
-
     <!-- Posts / Favorites -->
     <section class="mb-20 w-full">
       <div class="flex items-center">
