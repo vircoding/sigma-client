cf763da15c3decf4b0525c32fab88ff87f0c01f7
PVIdubs <nluismi80@gmail.com>
2023-09-10 15:44:37 -0400

    update-client-form

--- src/components/ClientInfo.vue
+++ src/components/ClientInfo.vue
@@ -5,6 +5,7 @@
   import { useLayoutStore } from "../stores/layout";
   import SigmaIsotypeIcon from "./icons/SigmaIsotypeIcon.vue";
   import SmallPostCard from "./SmallPostCard.vue";
+  import UpdateClientForm from "./UpdateClientForm.vue";
 
   const userStore = useUserStore();
   const postStore = usePostStore();
@@ -21,42 +22,11 @@
     if (updateFavoritesController.value) await userStore.loadUserFavorites();
   });
 
-  const newUser = ref({
-    username: userStore.userState.info.username,
-  });
-
-  const usernameError = computed(() => {
-    if (!(newUser.value.username.length >= 3 && newUser.value.username.length <= 20)) return true;
-    return false;
-  });
-
-  const disableSubmit = computed(() => {
-    if (usernameError.value) {
-      return true;
-    }
-    if (newUser.value.username === userStore.userState.info.username) return true;
-    return false;
-  });
-
   const logoutEvent = async () => {
     try {
       updateFavoritesController.value = false;
       await userStore.logoutUser();
       updateFavoritesController.value = true;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const $reset = () => {
-    newUser.value = {
-      username: userStore.userState.info.username,
-    };
-  };
-
-  const formSubmit = async () => {
-    try {
-      await userStore.updateClient(newUser.value);
     } catch (error) {
       console.log(error);
     }
@@ -122,46 +92,9 @@
     <div class="text-shadow col-span-6">
       <SigmaIsotypeIcon class="h-[100px] w-[320px] fill-sgray-200 lg:hidden" />
     </div>
-    <form
-      @submit.prevent="formSubmit"
-      novalidate
-      class="mb-7 flex w-full grid-cols-2 grid-rows-6 flex-col lg:mb-0 lg:grid lg:gap-x-8 lg:gap-y-2"
-    >
-      <!-- Username -->
-      <div class="mb-1 w-full">
-        <input
-          type="text"
-          v-model.trim="newUser.username"
-          class="w-full rounded-md border border-sgray-100 bg-transparent px-4 py-2 font-medium transition-colors duration-200 placeholder:text-sgray-200 hover:border-sgray-300 hover:bg-gray-100 focus:border-transparent focus:bg-gray-100 focus:shadow-[0_2px_10px_rgba(0,_0,_0,_0.4)] focus:outline-none focus:ring-1 lg:text-lg"
-          :class="
-            usernameError
-              ? 'border-transparent ring-2 ring-alert hover:border-transparent focus:border-transparent focus:ring-2'
-              : 'ring-sigma'
-          "
-          placeholder="Nombre de Usuario"
-        />
-        <span
-          class="text-shadow top-1 px-4 font-archivo text-sm italic text-alert"
-          :class="usernameError ? 'visible' : 'invisible'"
-          >Error Message</span
-        >
-      </div>
-      <div class="flex grow flex-row gap-3">
-        <button
-          class="flex h-[38px] w-full items-center justify-center rounded-md border border-sgray-400 bg-sgray-400 text-center font-semibold text-sgray-100 shadow-[0_0_10px_rgba(0,_0,_0,_0.4)] transition-all duration-200 ease-out hover:bg-black hover:text-white hover:shadow-[0_2px_10px_rgba(0,_0,_0,_0.5)] disabled:border disabled:border-sgray-100 disabled:bg-transparent disabled:font-normal disabled:text-sgray-200 disabled:shadow-[0_0_10px_rgba(0,_0,_0,_0)] lg:h-10 lg:w-44 lg:text-lg"
-          @click.prevent="logoutEvent"
-        >
-          Cerrar Sesión
-        </button>
-        <button
-          type="submit"
-          :disabled="disableSubmit"
-          class="flex h-[38px] w-full items-center justify-center rounded-md border border-sgray-400 bg-sgray-400 text-center font-semibold text-sgray-100 shadow-[0_0_10px_rgba(0,_0,_0,_0.4)] transition-all duration-200 ease-out hover:bg-black hover:text-white hover:shadow-[0_2px_10px_rgba(0,_0,_0,_0.5)] disabled:border disabled:border-sgray-100 disabled:bg-transparent disabled:font-normal disabled:text-sgray-200 disabled:shadow-[0_0_10px_rgba(0,_0,_0,_0)] lg:h-10 lg:w-44 lg:text-lg"
-        >
-          Guardar
-        </button>
-      </div>
-    </form>
+
+    <!-- Form -->
+    <UpdateClientForm @logout="logoutEvent" />
 
     <!-- Posts / Favorites -->
     <section class="mb-20 w-full">
---
+++ src/components/UpdateClientForm.vue
@@ -0,0 +1,79 @@
+<script setup>
+  import { ref, watch, computed } from "vue";
+  import { useUserStore } from "../stores/user";
+
+  defineEmits(["logout"]);
+
+  const userStore = useUserStore();
+
+  const newUser = ref({
+    username: userStore.userState.info.username,
+  });
+
+  const errors = ref({
+    username: false,
+  });
+
+  watch(newUser.value, () => {
+    if (!(newUser.value.username.length >= 3 && newUser.value.username.length <= 20))
+      errors.value.username = true;
+    else errors.value.username = false;
+  });
+
+  const disableSubmit = computed(() => {
+    return errors.value.username || newUser.value.username === userStore.userState.info.username;
+  });
+
+  const formSubmit = async () => {
+    try {
+      await userStore.updateClient(newUser.value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+</script>
+
+<template>
+  <form
+    @submit.prevent="formSubmit"
+    novalidate
+    class="mb-7 flex w-full grid-cols-2 grid-rows-6 flex-col lg:mb-0 lg:grid lg:gap-x-8 lg:gap-y-2"
+  >
+    <!-- Username -->
+    <div class="mb-1 w-full">
+      <input
+        id="username"
+        autocomplete="username"
+        type="text"
+        v-model.trim="newUser.username"
+        class="w-full rounded-md border border-sgray-100 bg-transparent px-4 py-2 font-medium transition-colors duration-200 placeholder:text-sgray-200 hover:border-sgray-300 hover:bg-gray-100 focus:border-transparent focus:bg-gray-100 focus:shadow-[0_2px_10px_rgba(0,_0,_0,_0.4)] focus:outline-none focus:ring-1 lg:text-lg"
+        :class="
+          errors.username
+            ? 'border-transparent ring-2 ring-alert hover:border-transparent focus:border-transparent focus:ring-2'
+            : 'ring-sigma'
+        "
+        placeholder="Nombre de Usuario"
+      />
+      <span
+        :class="errors.username ? 'visible' : 'invisible'"
+        class="text-shadow top-1 px-4 font-archivo text-sm italic text-alert"
+        >Nombre de usuario Inválido</span
+      >
+    </div>
+    <div class="flex grow flex-row gap-3">
+      <button
+        class="flex h-[38px] w-full items-center justify-center rounded-md border border-sgray-400 bg-sgray-400 text-center font-semibold text-sgray-100 shadow-[0_0_10px_rgba(0,_0,_0,_0.4)] transition-all duration-200 ease-out hover:bg-black hover:text-white hover:shadow-[0_2px_10px_rgba(0,_0,_0,_0.5)] disabled:border disabled:border-sgray-100 disabled:bg-transparent disabled:font-normal disabled:text-sgray-200 disabled:shadow-[0_0_10px_rgba(0,_0,_0,_0)] lg:h-10 lg:w-44 lg:text-lg"
+        @click.prevent="$emit('logout')"
+      >
+        Cerrar Sesión
+      </button>
+      <button
+        type="submit"
+        :disabled="disableSubmit"
+        class="flex h-[38px] w-full items-center justify-center rounded-md border border-sgray-400 bg-sgray-400 text-center font-semibold text-sgray-100 shadow-[0_0_10px_rgba(0,_0,_0,_0.4)] transition-all duration-200 ease-out hover:bg-black hover:text-white hover:shadow-[0_2px_10px_rgba(0,_0,_0,_0.5)] disabled:border disabled:border-sgray-100 disabled:bg-transparent disabled:font-normal disabled:text-sgray-200 disabled:shadow-[0_0_10px_rgba(0,_0,_0,_0)] lg:h-10 lg:w-44 lg:text-lg"
+      >
+        Guardar
+      </button>
+    </div>
+  </form>
+</template>
