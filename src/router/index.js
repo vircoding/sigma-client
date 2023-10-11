5c5426425192d5c668ceb228122091286de82a69
PVIdubs <nluismi80@gmail.com>
2023-10-11 18:59:46 -0400

    scroll-behavior

--- src/router/index.js
+++ src/router/index.js
@@ -281,6 +281,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { left: 0, top: 0 };
+    }
+  },
 });
 
 router.beforeEach(async (to, from, next) => {
