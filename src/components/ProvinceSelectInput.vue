a64c410ccc45bbb11cabd7416e546faf9b1d08b2
PVIdubs <nluismi80@gmail.com>
2023-11-02 17:26:03 -0400

    minor changes

--- src/components/ProvinceSelectInput.vue
+++ src/components/ProvinceSelectInput.vue
@@ -13,7 +13,7 @@
       :id="'province-' + index"
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
-      class="w-full appearance-none rounded-md border border-sgray-100 bg-transparent px-4 pb-[5px] pt-[7px] font-medium outline-none transition-colors duration-200 placeholder:text-sgray-200 hover:border-sgray-300 hover:bg-gray-100 focus:border-sgray-300 focus:border-transparent focus:bg-white focus:shadow-[0_2px_10px_rgba(0,_0,_0,_0.4)] focus:outline-none focus:ring-1 lg:text-lg"
+      class="w-full appearance-none rounded-md border border-sgray-100 bg-transparent px-4 pb-[5px] pt-[7px] outline-none transition-colors duration-200 focus:border-sgray-300 focus:bg-white"
     >
       <option v-for="(item, index) in provinceList" :key="index" :value="item">{{ item }}</option>
     </select>
