853ff4a777f8b89786659179602ad20316f922a1
PVIdubs <nluismi80@gmail.com>
2024-01-08 10:43:29 -0500

    contact-view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,3 +1,22 @@
+<script setup>
+  import NavBar from "../components/NavBar.vue";
+  import FooterSection from "../components/FooterSection.vue";
+</script>
+
 <template>
-  <h1>Contact View</h1>
+  <div class="lg:px-2.5">
+    <div class="flex min-h-screen flex-col lg:h-screen lg:py-2.5">
+      <header>
+        <div class="relative overflow-hidden bg-background">
+          <NavBar class="z-20" />
+        </div>
+      </header>
+      <main class="mb-[5px] grow bg-background lg:mb-0">
+        <h6>Contact View</h6>
+      </main>
+    </div>
+    <footer class="lg:mb-2.5">
+      <FooterSection background="#333333" text="#dcdcdc" title="#ffffff" />
+    </footer>
+  </div>
 </template>
