c13c0d797d46177cabed6f570f374d8cb59602ae
PVIdubs <nluismi80@gmail.com>
2024-02-01 09:55:26 -0500

    help-index-section

---
+++ src/components/HelpIndexSection.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <!-- Container -->
+  <div
+    class="mb-[5px] bg-background pb-10 pt-9 text-center max-[1023px]:px-[10%] max-[499px]:px-[5%]"
+  >
+    <!-- Border -->
+    <div
+      class="text-shadow w-full overflow-hidden rounded-2xl border border-sgray-100 bg-sigma pt-3"
+    >
+      <!-- Index -->
+      <ul class="flex w-full flex-col justify-center bg-white px-7 text-left">
+        <li class="flex w-full items-center justify-between border-b border-sgray-100 pb-3 pt-4">
+          <h2 class="text-base font-semibold text-sgray-400">Índice</h2>
+          <img src="../assets/down-arrow.svg" class="w-3" />
+        </li>
+
+        <!-- Video Tutorial -->
+        <li class="w-full border-b border-sgray-100 py-3">
+          <a href="" class="text-base text-sgray-300"
+            ><span class="font-semibold">1.</span> Video Tutorial</a
+          >
+        </li>
+
+        <!-- Tutorial Paso a Paso -->
+        <li class="w-full border-b border-sgray-100 py-3">
+          <a href="" class="text-base text-sgray-300"
+            ><span class="font-semibold">2.</span> Tutorial Paso a Paso</a
+          >
+        </li>
+
+        <!-- Contratar a un agente -->
+        <li class="w-full border-b border-sgray-100 py-3">
+          <a href="" class="text-base text-sgray-300"
+            ><span class="font-semibold">3.</span> Contratar a un agente</a
+          >
+        </li>
+
+        <!-- Preguntas frecuentes -->
+        <li class="w-full border-b border-sgray-100 pb-4 pt-3">
+          <a href="" class="text-base text-sgray-300"
+            ><span class="font-semibold">4.</span> Preguntas frecuentes</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
