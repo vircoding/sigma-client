654ba788a881434b5c0979452e9b2ade8d5955f3
PVIdubs <nluismi80@gmail.com>
2024-02-01 10:28:48 -0500

    help-view

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,5 +1,10 @@
 <script setup>
   import NavBar from "../components/NavBar.vue";
+  import HelpIndexSection from "../components/HelpIndexSection.vue";
+  import VideoTutorialSection from "../components/VideoTutorialSection.vue";
+  import TextTutorialSection from "../components/TextTutorialSection.vue";
+  import HelpAgentSection from "../components/HelpAgentSection.vue";
+  import FAQ from "../components/FAQ.vue";
   import FooterSection from "../components/FooterSection.vue";
 </script>
 
@@ -11,12 +16,47 @@
           <NavBar class="z-20" />
         </div>
       </header>
-      <main class="mb-[5px] grow bg-background lg:mb-0">
-        <h6>Index</h6>
-        <h6 id="video">Video Tutorial Section</h6>
-        <h6 id="tutorial">Text Tutorial Section</h6>
-        <h6 id="agent">Agent Section</h6>
-        <h6 id="faq">FAQ Section</h6>
+      <main class="mb-[5px] flex grow flex-col bg-background lg:mb-0">
+        <div class="mb-[5px] bg-background text-center max-[1023px]:px-[10%] max-[499px]:px-[5%]">
+          <!-- Hero -->
+          <div class="pt-10">
+            <h2
+              class="text-shadow mb-5 text-center text-4xl font-extrabold max-[1023px]:text-4xl max-[505px]:text-3xl lg:mb-0 lg:text-left lg:text-4xl"
+            >
+              Ayuda
+            </h2>
+            <p
+              class="text-shadow block text-center text-base text-sgray-300 lg:text-left lg:text-lg"
+            >
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque omnis odio amet?
+            </p>
+          </div>
+        </div>
+
+        <!-- Index -->
+        <section id="index">
+          <HelpIndexSection />
+        </section>
+
+        <!-- Video Tutorial -->
+        <section id="videotut">
+          <VideoTutorialSection />
+        </section>
+
+        <!-- Text Tutorial -->
+        <section id="texttut">
+          <TextTutorialSection />
+        </section>
+
+        <!-- Find Agent -->
+        <section id="findagent">
+          <HelpAgentSection />
+        </section>
+
+        <!-- FAQ -->
+        <section id="faq">
+          <FAQ />
+        </section>
       </main>
     </div>
     <footer class="lg:mb-2.5">
