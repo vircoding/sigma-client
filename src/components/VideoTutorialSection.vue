1d525d12356cd966df3c9074f60d921e0b71b5ba
PVIdubs <nluismi80@gmail.com>
2024-02-01 10:17:56 -0500

    video-tutorial-section

---
+++ src/components/VideoTutorialSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="mb-[5px] overflow-hidden bg-white text-center max-[1023px]:px-[10%] max-[499px]:px-[5%]"
+  >
+    <!-- Hero -->
+    <div class="py-10">
+      <h2
+        class="text-shadow mb-5 text-center text-4xl font-extrabold max-[1023px]:text-4xl max-[505px]:text-3xl lg:mb-0 lg:text-left lg:text-4xl"
+      >
+        Video Tutorial
+      </h2>
+      <p class="text-shadow block text-center text-sgray-300 lg:text-left lg:text-lg">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, aliquid.
+      </p>
+    </div>
+
+    <!-- Video -->
+    <div class="w-full pb-12">
+      <figure class="relative flex aspect-video w-full flex-col items-center justify-center">
+        <div class="absolute aspect-video w-full rounded-2xl bg-sgray-400"></div>
+        <img src="../assets/color-youtube-icon.svg" class="z-10 w-20" />
+      </figure>
+    </div>
+  </div>
+</template>
