0f66b49e4443573b06a710a8056a107b58c3480c
vircoding <nluismi80@gmail.com>
2024-02-10 01:03:39 -0500

    fixing bugs

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,3 @@
-<script setup>
-  import NotFoundIcon from "../components/icons/NotFoundIcon.vue";
-</script>
-
 <template>
   <div class="flex min-h-screen flex-col items-center justify-center gap-10 text-center">
     <img src="../assets/sigma-404-icon.svg" class="w-1/2" />
