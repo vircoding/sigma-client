dc037e44084c3628e33906a3acbb745a7ec301b9
vircoding <nluismi80@gmail.com>
2024-03-09 13:38:17 -0500

    improving footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -23,12 +23,12 @@
       :style="`color: ${props.text}; background-color: ${props.background}`"
     >
       <!-- Top -->
-      <div class="mb-8 flex w-full items-center justify-start gap-y-6">
+      <div class="mb-10 flex w-full items-center justify-start gap-y-6">
         <!-- Left -->
         <div class="w-3/5 space-y-4">
           <!-- Company -->
           <div class="flex w-full flex-col">
-            <h6 class="text-base font-semibold tracking-wide">EMPRESA</h6>
+            <h6 class="text-lg font-semibold tracking-wide">EMPRESA</h6>
             <RouterLink to="/support/about#who" class="hover:underline">¿Quiénes somos?</RouterLink>
             <RouterLink to="/support/about#team" class="hover:underline">Nuestro equipo</RouterLink>
             <RouterLink to="/support/about#agent" class="hover:underline"
@@ -38,7 +38,7 @@
 
           <!-- Help -->
           <div class="flex w-full flex-col">
-            <h6 class="text-base font-semibold tracking-wide">AYUDA</h6>
+            <h6 class="text-lg font-semibold tracking-wide">AYUDA</h6>
             <RouterLink to="/support/help#videotut" class="hover:underline"
               >¿Cómo funciona el sitio?</RouterLink
             >
@@ -52,35 +52,26 @@
 
           <!-- Contact Us -->
           <div class="flex w-full flex-col">
-            <h6 class="text-base font-semibold tracking-wide">CONTÁCTANOS</h6>
-            <a href="mailto:sigmacuba2023@gmail.com">sigmacuba2023@gmail.com</a>
+            <h6 class="text-lg font-semibold tracking-wide">CONTÁCTANOS</h6>
+            <a
+              href="mailto:sigmacuba2023@gmail.com"
+              class="flex items-center gap-1 hover:underline"
+            >
+              <img src="../assets/email-icon.svg" />
+              <span class="relative top-[1px]">Correo Electrónico</span>
+            </a>
             <a href="https://wa.me/+5350009047" class="flex items-center gap-1 hover:underline">
               <WhatsappIcon :color="sigmaIconFill" />
-              <span>50009047</span>
+              <span class="relative top-[1px]">Whatsapp</span>
             </a>
           </div>
         </div>
 
         <!-- Rigth -->
         <div class="flex w-2/5 flex-col items-center justify-center">
-          <!-- Powered By -->
-          <div class="mb-4 w-full pl-6 text-center">
-            <h6 class="mb-3 text-base font-semibold tracking-wide">POWERED BY</h6>
-            <div class="grid grid-cols-2 grid-rows-4 place-items-center gap-y-2">
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-              <div class="h-6 w-6 rounded-md bg-stone-300"></div>
-            </div>
-          </div>
-
           <!-- Socials -->
           <div class="w-full pl-6 text-center">
-            <h6 class="mb-3 text-base font-semibold tracking-wide">SÍGUENOS</h6>
+            <h6 class="mb-3 text-lg font-semibold tracking-wide">SÍGUENOS</h6>
             <div class="grid grid-cols-3 grid-rows-2 place-items-center gap-y-2">
               <!-- Instagram -->
               <a href="https://www.instagram.com/sigmacuba/">
@@ -117,7 +108,7 @@
       </div>
       <!-- Copyrigths -->
       <div
-        class="flex w-full items-center justify-evenly pb-11"
+        class="flex w-full items-center justify-evenly pb-12"
         :style="`color: ${props.text}; background-color: ${props.background}`"
       >
         <SigmaIcon class="w-20 lg:w-56" :class="sigmaIconFill" />
