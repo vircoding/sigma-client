180dec5d968119c733e9c8db4b3d10e1f5f423ad
vircoding <nluismi80@gmail.com>
2024-03-09 14:32:47 -0500

    new side-menu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -28,13 +28,55 @@
 </script>
 
 <template>
-  <nav class="z-50 flex h-screen w-1/2 flex-col items-end text-sgray-400">
+  <nav class="z-50 flex h-screen w-3/5 flex-col items-end text-sgray-400">
     <div class="flex h-[68px] items-center p-4 lg:hidden">
       <img src="../assets/close-icon.svg" class="text-shadow w-5" @click="menuInteraction" />
     </div>
     <div class="h-[1px] w-full bg-sgray-100"></div>
-    <ul class="flex flex-col items-end gap-6 pr-6 pt-10">
-      <li v-if="userStore.isLoggedIn">
+    <ul class="flex flex-col items-end gap-6 pr-5 pt-10">
+      <!-- Contact -->
+      <li class="flex w-full items-center justify-end gap-[6px]">
+        <span class="text-shadow relative font-poppins text-lg font-semibold text-sblue-500"
+          >Contáctanos</span
+        >
+        <img src="../assets/side-menu-contact-icon.svg" class="w-6" />
+      </li>
+
+      <!-- Help -->
+      <li class="flex w-full items-center justify-end gap-[6px]">
+        <span class="text-shadow relative font-poppins text-lg font-semibold text-sblue-500"
+          >Ayuda</span
+        >
+        <img src="../assets/side-menu-help-icon.svg" class="w-6" />
+      </li>
+
+      <!-- Register -->
+      <li class="flex w-full items-center justify-end gap-[6px]">
+        <span class="text-shadow relative font-poppins text-lg font-semibold text-sblue-500"
+          >Registrarse</span
+        >
+        <img src="../assets/side-menu-register-icon.svg" class="w-6" />
+      </li>
+
+      <!-- Login -->
+      <li class="flex w-full items-center justify-end gap-[6px]">
+        <span class="text-shadow relative font-poppins text-lg font-semibold text-sblue-500"
+          >Iniciar Sesión</span
+        >
+        <img src="../assets/side-menu-login-icon.svg" class="w-6" />
+      </li>
+
+      <!-- Home -->
+      <li class="flex w-full items-center gap-[6px]">
+        <RouterLink
+          to="/home"
+          class="text-shadow relative top-[2px] font-poppins text-lg font-semibold text-sblue-500"
+          >Página Principal</RouterLink
+        >
+        <img src="../assets/side-menu-home-icon.svg" class="w-6" />
+      </li>
+
+      <!-- <li v-if="userStore.isLoggedIn">
         <RouterLink to="/find" class="text-shadow font-poppins text-lg">Compra</RouterLink>
       </li>
       <li v-if="userStore.isLoggedIn">
@@ -65,7 +107,7 @@
         <RouterLink to="/account" class="text-shadow font-poppins text-lg font-bold"
           >Mi Cuenta</RouterLink
         >
-      </li>
+      </li> -->
     </ul>
   </nav>
 </template>
