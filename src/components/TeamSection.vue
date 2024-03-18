410b3a88d153e5390abec6949b5301f1c65768fb
vircoding <nluismi80@gmail.com>
2024-03-18 15:27:34 -0400

    new about-view

--- src/components/TeamSection.vue
+++ src/components/TeamSection.vue
@@ -1,29 +1,26 @@
 <template>
-  <div
-    class="mb-[5px] overflow-hidden bg-background text-center max-[1023px]:px-[10%] max-[499px]:px-[5%]"
-  >
+  <div class="overflow-hidden bg-white py-10 text-center">
     <!-- Hero -->
-    <div class="py-10">
-      <h2
-        class="text-shadow mb-5 text-center text-4xl font-extrabold max-[1023px]:text-4xl max-[505px]:text-3xl lg:mb-0 lg:text-left lg:text-4xl"
-      >
+    <div class="mb-6 flex w-full flex-col gap-2 px-5 text-center">
+      <h2 class="text-shadow w-full font-ubuntu text-3xl font-bold text-sblue-500">
         Nuestro equipo
       </h2>
-      <p class="text-shadow block text-center text-sgray-300 lg:text-left lg:text-lg">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, aliquid.
+      <p class="text-shadow w-full px-2 text-sm text-sgray-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa beatae explicabo qui.
       </p>
     </div>
+
     <!-- Main -->
-    <ul class="w-full space-y-10 pb-12">
+    <ul class="w-full space-y-10">
       <!-- Enmanuel Diez -->
       <li class="relative right-8 flex w-full flex-col items-center justify-center text-center">
         <figure
-          class="text-shadow w-1/2 overflow-hidden rounded-full border-2 border-white min-[450px]:w-2/5"
+          class="text-shadow w-1/2 overflow-hidden rounded-full border border-sgray-100 min-[450px]:w-2/5"
         >
           <img src="../assets/agent-avatar.jpg" />
         </figure>
         <div class="text-shadow mt-3 rounded-2xl border-sgray-200 bg-white px-6 py-1">
-          <h5 class="text-2xl font-semibold">Enmanuel Diez</h5>
+          <h5 class="text-2xl font-semibold text-sblue-500">Enmanuel Diez</h5>
           <h6 class="mt-[1px] text-base text-sgray-300">Cofundador & CEO</h6>
           <a href="" class="flex items-center justify-center gap-[2px]">
             <img src="../assets/instagram-icon.svg" />
@@ -35,12 +32,12 @@
       <!-- Lawrence AlAssal -->
       <li class="relative left-8 flex w-full flex-col items-center justify-center text-center">
         <figure
-          class="text-shadow w-1/2 overflow-hidden rounded-full border-2 border-white min-[450px]:w-2/5"
+          class="text-shadow w-1/2 overflow-hidden rounded-full border border-sgray-100 min-[450px]:w-2/5"
         >
           <img src="../assets/agent-avatar.jpg" />
         </figure>
         <div class="text-shadow mt-3 rounded-2xl border-sgray-200 bg-white px-6 py-1">
-          <h5 class="text-2xl font-semibold">Lawrence AlAssal</h5>
+          <h5 class="text-2xl font-semibold text-sblue-500">Lawrence AlAssal</h5>
           <h6 class="mt-[1px] text-base text-sgray-300">Cofundador & COO</h6>
           <a href="" class="flex items-center justify-center gap-[2px]">
             <img src="../assets/instagram-icon.svg" />
@@ -52,12 +49,12 @@
       <!-- Patricia Arenas -->
       <li class="relative right-8 flex w-full flex-col items-center justify-center text-center">
         <figure
-          class="text-shadow w-1/2 overflow-hidden rounded-full border-2 border-white min-[450px]:w-2/5"
+          class="text-shadow w-1/2 overflow-hidden rounded-full border border-sgray-100 min-[450px]:w-2/5"
         >
           <img src="../assets/agent-avatar.jpg" />
         </figure>
         <div class="text-shadow mt-3 rounded-2xl border-sgray-200 bg-white px-6 py-1">
-          <h5 class="text-2xl font-semibold">Patricia Arenas</h5>
+          <h5 class="text-2xl font-semibold text-sblue-500">Patricia Arenas</h5>
           <h6 class="mt-[1px] text-base text-sgray-300">CFO</h6>
           <a href="" class="flex items-center justify-center gap-[2px]">
             <img src="../assets/instagram-icon.svg" />
@@ -69,12 +66,12 @@
       <!-- Luis Miguel Navarro -->
       <li class="relative left-8 flex w-full flex-col items-center justify-center text-center">
         <figure
-          class="text-shadow w-1/2 overflow-hidden rounded-full border-2 border-white min-[450px]:w-2/5"
+          class="text-shadow w-1/2 overflow-hidden rounded-full border border-sgray-100 min-[450px]:w-2/5"
         >
           <img src="../assets/agent-avatar.jpg" />
         </figure>
         <div class="text-shadow mt-3 rounded-2xl border-sgray-200 bg-white px-6 py-1">
-          <h5 class="text-2xl font-semibold">Luis Miguel Navarro</h5>
+          <h5 class="text-2xl font-semibold text-sblue-500">Luis Miguel Navarro</h5>
           <h6 class="mt-[1px] text-base text-sgray-300">Desarrollador & CTO</h6>
           <a href="" class="flex items-center justify-center gap-[2px]">
             <img src="../assets/instagram-icon.svg" />
@@ -86,12 +83,12 @@
       <!-- Isabela Díaz -->
       <li class="relative right-8 flex w-full flex-col items-center justify-center text-center">
         <figure
-          class="text-shadow w-1/2 overflow-hidden rounded-full border-2 border-white min-[450px]:w-2/5"
+          class="text-shadow w-1/2 overflow-hidden rounded-full border border-sgray-100 min-[450px]:w-2/5"
         >
           <img src="../assets/agent-avatar.jpg" />
         </figure>
         <div class="text-shadow mt-3 rounded-2xl border-sgray-200 bg-white px-6 py-1">
-          <h5 class="text-2xl font-semibold">Isabela Díaz</h5>
+          <h5 class="text-2xl font-semibold text-sblue-500">Isabela Díaz</h5>
           <h6 class="mt-[1px] text-base text-sgray-300">Community Manager & CMO</h6>
           <a href="" class="flex items-center justify-center gap-[2px]">
             <img src="../assets/instagram-icon.svg" />
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,44 +1,35 @@
 <script setup>
   import NavBar from "../components/NavBar.vue";
-  import WhoSection from "../components/WhoSection.vue";
   import TeamSection from "../components/TeamSection.vue";
-  import ReviewsSection from "../components/ReviewsSection.vue";
   import FooterSection from "../components/FooterSection.vue";
-  import AboutAgentSection from "../components/AboutAgentSection.vue";
 </script>
 
 <template>
-  <div class="lg:px-2.5">
-    <div class="flex min-h-screen flex-col lg:h-screen lg:py-2.5">
-      <header>
-        <div class="relative overflow-hidden bg-background">
-          <NavBar class="z-20" />
-        </div>
-      </header>
-      <main class="mb-[5px] flex grow flex-col bg-background lg:mb-0">
-        <!-- Who Section -->
-        <section id="who">
-          <WhoSection />
-        </section>
+  <!-- NavBar -->
+  <header class="relative z-20 h-[68px] w-full shadow-md">
+    <NavBar class="absolute" />
+  </header>
 
-        <!-- Team Section -->
-        <section id="team">
-          <TeamSection />
-        </section>
-
-        <!-- Agent Section -->
-        <section id="agent">
-          <AboutAgentSection />
-        </section>
-
-        <!-- Reviews Section -->
-        <section id="feedback">
-          <ReviewsSection />
-        </section>
-      </main>
+  <main class="w-full">
+    <!-- Hero -->
+    <div id="who" class="flex w-full flex-col gap-2 px-5 py-10 text-center">
+      <h2 class="text-shadow w-full font-ubuntu text-3xl font-bold text-sblue-500">
+        ¿Quiénes somos?
+      </h2>
+      <p class="text-shadow w-full px-2 text-sm text-sgray-300">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio praesentium in reprehenderit
+        itaque asperiores repellat unde vitae labore sed odit.
+      </p>
     </div>
-    <footer class="lg:mb-2.5">
-      <FooterSection background="#333333" text="#dcdcdc" title="#ffffff" />
-    </footer>
-  </div>
+
+    <!-- Horizontal Line -->
+    <div class="w-full border-b border-sgray-100"></div>
+
+    <section id="team" class="w-full">
+      <TeamSection />
+    </section>
+  </main>
+
+  <!-- Footer -->
+  <FooterSection background="#011b33" title="#ffffff" text="#ffffff" />
 </template>
